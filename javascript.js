1b94edbd03a62af2cca92063028f6a56b4ea1f9c
Daniel Fernandez <danielfe@uvic.ca>
2022-03-19 13:33:56 -0700

    Initial commit to UI branch

--- javascript.js
+++ javascript.js
@@ -17,83 +17,142 @@ function computerPlay() {
 
 function playOneRound(playerSelection, computerSelection) {
 
-    playerSelection = prompt("Rock, Paper, Scissors?");
+    
     playerSelection=playerSelection.toUpperCase();
     computerSelection=computerPlay();
 
     let result = "";
 
+    if (playerScore==5 || computerScore==5) {
+    
+        const divContainer =document.querySelector(".content");
+        let gameOver= document.createElement("p");
+        gameOver.classList.add('gameOver');
+        gameOver.textContent = `Game over!. Your final score is ${playerScore}. The computer score is ${computerScore}. Play Again?`;
+        content.appendChild(gameOver);
+        playerScore=0;
+        computerScore=0;
+    }
 
 
     if (playerSelection==computerSelection) {
-        return result = "Draw!";
+        const divContainer =document.querySelector(".content");
+        let result = document.createElement("p");
+        result.classList.add('result');
+        result.textContent = `Draw! Your current score is ${playerScore}. The computer score is ${computerScore}`;
+        content.appendChild(result);
+        return;
+        
     }
 
     if (playerSelection=="ROCK" && computerSelection=="SCISSORS") {
 
         playerScore=playerScore+1;
-        return result = "You win! Rock beats scissors.";
+        const divContainer =document.querySelector(".content");
+        let result = document.createElement("p");
+        result.classList.add('result');
+        result.textContent = `You win! Rock beats scissors.Your current score is ${playerScore}. The computer score is ${computerScore}`;
+        content.appendChild(result);
+        return;
+
     }
 
     if (playerSelection=="PAPER" && computerSelection=="ROCK") {
 
         playerScore=playerScore+1;
-        return result = "You win! Paper beats rock"
+        const divContainer =document.querySelector(".content");
+        let result = document.createElement("p");
+        result.classList.add('result');
+        result.textContent = `You win! Paper beats rock.Your current score is ${playerScore}. The computer score is ${computerScore}`;
+        content.appendChild(result);
+        return;
     }
 
     if (playerSelection=="SCISSORS" && computerSelection=="PAPER") {
 
         playerScore=playerScore+1;
-        return result = "You win! Scissors beats paper."
+        const divContainer =document.querySelector(".content");
+        let result = document.createElement("p");
+        result.classList.add('result');
+        result.textContent = `You win! Scissors beats paper.Your current score is ${playerScore}. The computer score is ${computerScore}`;
+        content.appendChild(result);
+        return;
     }
 
     if (playerSelection=="ROCK" && computerSelection=="PAPER") {
 
         computerScore=computerScore+1;
-        return result = "You Lose! Paper beats rock"
+        const divContainer =document.querySelector(".content");
+        let result = document.createElement("p");
+        result.classList.add('result');
+        result.textContent = `You Lose! Paper beats rock.Your current score is ${playerScore}. The computer score is ${computerScore}`;
+        content.appendChild(result);
+        return;
     }
 
     if (playerSelection=="PAPER" && computerSelection=="SCISSORS") {
 
+        
         computerScore=computerScore+1;
-        return result = "You Lose! Scissors beats paper."
+        const divContainer =document.querySelector(".content");
+        let result = document.createElement("p");
+        result.classList.add('result');
+        result.textContent = `You Lose! Scissors beats paper.Your current score is ${playerScore}. The computer score is ${computerScore}`;
+        content.appendChild(result);
+        return;
     }
 
     if (playerSelection=="SCISSORS" && computerSelection=="ROCK") {
 
         computerScore=computerScore+1;
-        return result = "You Lose! Rock beats scissors."
+        const divContainer =document.querySelector(".content");
+        let result = document.createElement("p");
+        result.classList.add('result');
+        result.textContent = `You Lose! Rock beats scissors. Your current score is ${playerScore}. The computer score is ${computerScore}`;
+        content.appendChild(result);
+        return;
     }
 
 
 }
 
 
-function game() {
+/*function game() {
 
     for(let i=0; i<5; i++) {
     console.log(playOneRound(playerSelection, computerSelection));
 
     }
+ 
+} */
+const rockBtn =document.querySelector('#rockBtn');
 
-}
+
+rockBtn.addEventListener('click', function (e) {
+    playOneRound("ROCK", computerSelection);
+  });
 
 
-game();
+const paperBtn = document.querySelector("#paperBtn");
 
-if (playerScore>computerScore) {
+paperBtn.addEventListener('click', function (e) {
+    playOneRound("PAPER", computerSelection);
+});
 
-    const scores = `Final scores: Your Score is ${playerScore} and the computer score is ${computerScore}. You win!`;
+const scissorsBtn = document.querySelector("#scissorsBtn");
 
-    console.log(scores);
+scissorsBtn.addEventListener('click', function (e) {
+    playOneRound("SCISSORS", computerSelection);
+});
 
-}
 
-else if (playerScore<computerScore) {
 
-    const scores = `Final scores: Your Score is ${playerScore} and the computer score is ${computerScore}. You lose.`;
 
-    console.log(scores);
 
-}
+const container = document.querySelector('#container');
 
+let content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'Start a new game by selecting rock, paper, or scissors!';
+
+container.appendChild(content);
